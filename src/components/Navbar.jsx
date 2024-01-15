cdd77e1c4824e60e366fcea462089272525ba86d
antorahad <antorahad6@gmail.com>
2024-01-15 11:18:36 +0600

    little modification

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../AuthProvider/AuthProvider";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
 
 const Navbar = () => {
     const { user, logOut } = useContext(AuthContext);
+
+    const [toggle, setToggle] = useState(false)
+
+    const handleToggle = () => {
+        setToggle(!toggle);
+    }
 
     const handleLogOut = () => {
         logOut();
@@ -17,10 +25,10 @@ const Navbar = () => {
                     </Link>
                 </div>
                 <div className="navbar-end">
-                    <ul className="flex items-center justify-center gap-5">
-                        <li><Link to={'/'}>Add employee</Link></li>
-                        <li><Link to={'/employees'}>Manage employee</Link></li>
-                        <li>
+                    <ul className="hidden lg:flex items-center justify-center gap-5">
+                        <li className="px-3"><Link to={'/'}>Add employee</Link></li>
+                        <li className="px-3"><Link to={'/employees'}>Manage employee</Link></li>
+                        <li className="px-3">
                             {
                                 user ?
 
@@ -35,6 +43,35 @@ const Navbar = () => {
                             }
                         </li>
                     </ul>
+                    <button onClick={handleToggle} className="flex lg:hidden items-center">
+                        {
+                            toggle ?
+                                <AiOutlineClose size={25}></AiOutlineClose>
+                                :
+                                <AiOutlineMenu size={25}></AiOutlineMenu>
+                        }
+                    </button>
+                    {
+                        toggle &&
+                        <ul className="flex items-center justify-center flex-col p-5 min-h-screen w-1/2 bg-slate-950 bg-opacity-70 absolute left-0 top-0">
+                            <li className="py-3"><Link to={'/'}>Add employee</Link></li>
+                            <li className="py-3"><Link to={'/employees'}>Manage employee</Link></li>
+                            <li className="py-3">
+                                {
+                                    user ?
+
+                                        <div className="flex flex-col items-center gap-3">
+                                            <p>{user.email}</p>
+                                            <button onClick={handleLogOut} className="btn btn-error text-white rounded-sm">Logout</button>
+                                        </div>
+                                        :
+                                        <Link to={'/login'}>
+                                            <button className="btn btn-success text-white rounded-sm">Login</button>
+                                        </Link>
+                                }
+                            </li>
+                        </ul>
+                    }
                 </div>
             </div>
         </div>
